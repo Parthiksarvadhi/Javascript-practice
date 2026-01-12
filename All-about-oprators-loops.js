da12be4ccc16fc44ac9d7049f96e6430779ba249
Sarvadhi Solution <sarvadhisolution@Sarvadhis-MacBook-Air.local>
2026-01-12 21:50:24 +0530

    loops and while

---
+++ All-about-oprators-loops.js
@@ -0,0 +1,9 @@
+const arr = [50,55,80,90,45];
+
+for(let i=0;i<marks.length;i++){
+    let sum=0;
+    if(arr[1]%2===0){
+        sum+=arr[i];
+        console.log("even numbers sum:",sum);
+    }
+}
